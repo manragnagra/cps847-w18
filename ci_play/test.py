6ea456e06bb134f1089e8d6f18cc9be5e05b2a80
AVM <miranska@users.noreply.github.com>
2018-02-06 17:42:01 -0500

    ci trigger

--- ci_play/test.py
+++ ci_play/test.py
@@ -7,7 +7,7 @@ class TestMyFunc(unittest.TestCase):
     def setUp(self):
         pass
  
-    def test_increment_one_1(self):
+    def test_increment_one_1(self): 
         self.assertEqual( my_functions.increment_by_one(1), 2)
   
     def test_increment_one_2(self):
